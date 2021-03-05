491426c8c2812537fefb609b3fe33c361ff2ee18
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-05 10:01:24 +0100

    Sfruttata la classe dopo correzione

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var hamburger = $('.header-right > a');
 var hamburgerMenu = $('.hamburger-menu');
 var close = $('.hamburger-menu > a');
 
-hamburger.click( function() {
-    hamburgerMenu.toggle(true);
-});
+// hamburger.click( function() {
+//     hamburgerMenu.toggle(true);
+// });
 
+// close.click( function() {
+//     hamburgerMenu.toggle(false);
+// });
+
+hamburger.click( function() {
+    hamburgerMenu.addClass('active');
+});
 close.click( function() {
-    hamburgerMenu.toggle(false);
+    hamburgerMenu.removeClass('active');
 });
